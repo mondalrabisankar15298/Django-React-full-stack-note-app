e6793e19fd182090ebdaa37fdadc27e3f09941c5
Rabi Sankar Mondal <rabisankarmondal@Rabis-MacBook-Air.local>
2025-04-28 12:05:44 +0530

    2nd commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,16 @@
 import Form from "../components/Form";
 
-function Login () {
-    return <Form route = "/api/token/" method= "login" />
+function Login() {
+    const register = () => {
+        window.location.href = "/register";
+    }
+    return (<div>
+                <Form route="/api/token/" method="login" />
+                <button onClick={() => register()}>Sign Up</button>
+             </div>
+
+
+    );
 }
 
 export default Login;
